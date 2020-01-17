c37ab0754caf2a773b54459d93e415a770fe4b1b
JFL <jledenmat.prestataire@oui.sncf>
2020-01-17 14:51:40 +0100

    [JFL] - Correction TU

--- test/HesperidesIntegrationSpec.groovy
+++ test/HesperidesIntegrationSpec.groovy
@@ -528,27 +528,91 @@ class HesperidesIntegrationSpec extends Specification implements Helper {
                 modulePropertiesPath: "#${logicGroupName}#${subLogicGroup}#${moduleName}#${moduleVersion}#WORKINGCOPY"
             )
         then:
-            log(propertiesDiff)
-            propertiesDiff != [
+            /*propertiesDiff != [
                 only_left: [],
                 only_right: [],
                 common: [],
                 differing: []
-            ]
-            /*[
-                'only_left':[],
-                'only_right':[],
-                'common':[
-                    'name':'myPropertyName',
-                    'left':['finalValue':'myPropertyValue','defaultValue':'','storedValue':'','transformations':[]],
-                    'right':['finalValue':'myPropertyValue','defaultValue':'','storedValue':'','transformations':[]],
-                ],
-                'differing':[
-                    'name':'myPropertyName2',
-                    'left':['finalValue':'myPropertyValue1','defaultValue':'','storedValue':'','transformations':[]],
-                    'right':['finalValue':'myPropertyValue2','defaultValue':'','storedValue':'','transformations':[]],
-                ]
             ]*/
+            propertiesDiff == 
+            {
+              only_left=[
+              
+              ],
+              only_right=[
+              
+              ],
+              common=[
+                {
+                  left={
+                    finalValue=myPropertyValue,
+                    defaultValue=null,
+                    storedValue=myPropertyValue,
+                    transformations=[
+                    
+                    ]
+                  },
+                  right={
+                    finalValue=myPropertyValue,
+                    defaultValue=null,
+                    storedValue=myPropertyValue,
+                    transformations=[
+                    
+                    ]
+                  },
+                  name=myPropertyName
+                },
+                {
+                  left={
+                    finalValue=platform,
+                    defaultValue=null,
+                    storedValue={
+                      {
+                        hesperides.platform.name
+                      }
+                    },
+                    transformations=[
+                      PROPERTY_SUBSTITUTION_LEVEL_1
+                    ]
+                  },
+                  right={
+                    finalValue=platform,
+                    defaultValue=null,
+                    storedValue={
+                      {
+                        hesperides.platform.name
+                      }
+                    },
+                    transformations=[
+                      PROPERTY_SUBSTITUTION_LEVEL_1
+                    ]
+                  },
+                  name=LCM_vha_test_builtin_property
+                },
+                {
+                  left={
+                    finalValue=42,
+                    defaultValue=null,
+                    storedValue=42,
+                    transformations=[
+                    
+                    ]
+                  },
+                  right={
+                    finalValue=42,
+                    defaultValue=null,
+                    storedValue=42,
+                    transformations=[
+                    
+                    ]
+                  },
+                  name=LCM_vha_test_property
+                }
+              ],
+              differing=[
+              
+              ]
+            }
     }
 
     def "Can compare 2 deployed modules on different platforms"() {
